bc4fe222f4666bed8e26d5bd5a7e12c949b420aa
Rodrigo Abreu de Oliveira <rodrigo.abreu.ext@dasa.com.br>
2022-05-15 11:54:40 -0300

    feat: added node challangers files

---
+++ desafio node/javascript/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { Repository } = require('./repository');
+const app = express();
+
+app.get('/', async (_, res) => {
+
+  const selectSql = `SELECT * FROM people`;
+  const people = await Repository.query(selectSql);
+
+  const title = '<h1>Full Cycle Rocks!</h1>';
+  const list = `
+    <ul>
+      ${people.map(p => `<li>${p.name}</li>`).join('')}
+    </ul>
+  `;
+
+  res.send(title + list);
+});
+
+app.listen(3000, () => {
+  console.log('Running on port 3000');
+
+  const createSql = `
+    CREATE TABLE IF NOT EXISTS people (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(50), PRIMARY KEY (id));
+  `;
+  Repository.query(createSql);
+
+  const insertSql = `
+    INSERT INTO people (name) values ('Rodrigo'), ('Helena'), ('SchoolOfNet'), ('CodeEducation');
+  `;
+  Repository.query(insertSql);
+});
